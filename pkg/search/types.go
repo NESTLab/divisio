d93187f3b6f3c9928de8ce2b8b6ac7e9f7db4ce5
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-01-31 16:07:14 -0500

    Moved A* specific stuff to new pkg folder 'search'

---
+++ pkg/search/types.go
@@ -0,0 +1,12 @@
+package search
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    string // The value of the item; arbitrary.
+	priority int    // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
