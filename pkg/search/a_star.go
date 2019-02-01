bf580a163e8136ba00a9104cbb2059f84617268c
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-02-01 16:29:26 -0500

    Fixed reference issues, and added a ReconstructPath function

--- pkg/search/a_star.go
+++ pkg/search/a_star.go
@@ -25,7 +25,7 @@ func GCost(g graph.Graph, start string, end string) int {
 
 func AStarSearch(g graph.Graph, start, end string) (map[string]string, map[string]int) {
 	frontier := make(PriorityQueue, len(g.Nodes))
-	startNode := Item{
+	startNode := &Item{
 		Node:     g.GetNode(start),
 		priority: 0,
 	}
@@ -38,7 +38,7 @@ func AStarSearch(g graph.Graph, start, end string) (map[string]string, map[strin
 	costSoFar[start] = 0
 
 	for frontier.Len() > 0 {
-		current := frontier.Pop().(Item)
+		current := frontier.Pop().(*Item)
 		currentName := current.Node.Name
 
 		if current.Node.Name == end {
@@ -54,7 +54,7 @@ func AStarSearch(g graph.Graph, start, end string) (map[string]string, map[strin
 			if !inCostSoFar || newCost < costSoFar[toName] {
 				costSoFar[toName] = newCost
 				priority := newCost + HCost(g, start, end)
-				frontier.Push(Item{
+				frontier.Push(&Item{
 					Node:     g.GetNode(toName),
 					priority: priority,
 				})
@@ -64,4 +64,22 @@ func AStarSearch(g graph.Graph, start, end string) (map[string]string, map[strin
 	}
 
 	return cameFrom, costSoFar
+}
+
+func ReconstructPath(cameFrom map[string]string, start string, end string) []string {
+	current := end
+	path := make([]string, 0)
+
+	for current != start {
+		path = append(path, current)
+		current = cameFrom[current]
+	}
+	path = append(path, start)
+
+	for i := len(path)/2 - 1; i >= 0; i-- {
+		opp := len(path) - 1 - i
+		path[i], path[opp] = path[opp], path[i]
+	}
+
+	return path
 }
