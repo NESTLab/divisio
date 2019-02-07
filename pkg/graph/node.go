793d35ea0ce56c7dc663d921b50a76d929ddef88
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-02-07 16:40:03 -0500

    Added IsTask(), function comments

--- pkg/graph/node.go
+++ pkg/graph/node.go
@@ -1,5 +1,6 @@
 package graph
 
+//MakeNode is just a wrapper for the normal Node{} method of building
 func MakeNode(name string, difficulty int, rate int, x, y int) *Node {
 	return &Node{
 		Name:       name,
@@ -7,4 +8,9 @@ func MakeNode(name string, difficulty int, rate int, x, y int) *Node {
 		Rate:       rate,
 		Pos:        Location{x, y},
 	}
+}
+
+//
+func (n Node) IsTask() bool {
+	return n.Difficulty > 0
 }
