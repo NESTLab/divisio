5cf03105d5e4eb457e59547ca1da9c95f62d9e15
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-02-21 16:32:49 -0500

    Added functional betweenness tester

--- pkg/search/betweenness.go
+++ pkg/search/betweenness.go
@@ -1,19 +1,64 @@
 package search
 
-import "github.com/NESTLab/divisio.git/pkg/graph"
+import (
+	"github.com/NESTLab/divisio.git/pkg/graph"
+)
 
 func BetweennessSearch(g graph.Graph, start string) map[string]int {
 	nodeFlow := make(map[string]int)
-	nodeHierarchy := GetNodeHierarchy(g, start)
-	for levelIndex, level := range nodeHierarchy {
-		for _, name := range level {
-			nodeFlow[name] = levelIndex
+	//Get the hierarchy for the nodes. From the start node at i=0, the slice at i=1 has the nodes 1 jump away, i=2 has two jumps away, etc
+	nodeHierarchy := getNodeHierarchy(g, start)
+
+	//Reverse the hierarchy so all the furthest nodes are at the top and we can iterate correctly
+	for i := len(nodeHierarchy)/2 - 1; i >= 0; i-- {
+		opp := len(nodeHierarchy) - 1 - i
+		nodeHierarchy[i], nodeHierarchy[opp] = nodeHierarchy[opp], nodeHierarchy[i]
+	}
+
+	//Stores the nodes that have already been processed, to avoid duplication and to select edge weights correctly
+	processed := make([]string, 0)
+
+	//Start from the 'bottom' and work towards the start node
+	for _, level := range nodeHierarchy {
+		//Go through each node in that level
+		for _, nodeName := range level {
+
+			//Get the node object itself via it's name
+			node := g.GetNode(nodeName)
+			thisNodeFlow := nodeFlow[nodeName]
+
+			//Add the node's rate to it's flow value. Tasks will add a positive rate, crossroads will add zero
+			thisNodeFlow += node.Rate
+
+			//Stores the sum of the outward edges (outward determined by if the toNode has already been processed) so we
+			//can use the edge weights to determine the split
+			var unexploredEdgeCostSum float64
+
+			//Get the list of nodes to iterate over
+			nodeEdges := g.GetEdges(nodeName)
+			for _, edge := range nodeEdges {
+				//We only care about the weights to the nodes we haven't visited yet
+				if !levelContains(processed, edge.ToNode) && !levelContains(level, edge.ToNode) {
+					unexploredEdgeCostSum += float64(edge.Weight)
+				}
+			}
+
+			//Now we iterate over it again, updating the toNodes nodeFlow values with how much this spreads
+			for _, edge := range nodeEdges {
+				if !levelContains(processed, edge.ToNode) && !levelContains(level, edge.ToNode) {
+					percentFlow := float64(1.0 - (float64(edge.Weight) / unexploredEdgeCostSum))
+					nodeFlow[edge.ToNode] += int(percentFlow * float64(thisNodeFlow))
+				}
+			}
+
+			processed = append(processed, nodeName)
 		}
 	}
+
 	return nodeFlow
 }
 
-func GetNodeHierarchy(g graph.Graph, start string) [][]string {
+func getNodeHierarchy(g graph.Graph, start string) [][]string {
 	nodeHierarchy := make([][]string, 0)
 
 	level0 := make([]string, 0)
@@ -31,8 +76,8 @@ func GetNodeHierarchy(g graph.Graph, start string) [][]string {
 			}
 		}
 	}
-
-	return nodeHierarchy
+	shrunkNodeHierarchy := append(make([][]string, 0, 0), nodeHierarchy...)
+	return shrunkNodeHierarchy
 }
 
 func hierarchyContains(hierarchy [][]string, nodeToCheck string) bool {
@@ -41,6 +86,15 @@ func hierarchyContains(hierarchy [][]string, nodeToCheck string) bool {
 			if node == nodeToCheck {
 				return true
 			}
+		}
+	}
+	return false
+}
+
+func levelContains(level []string, nodeToCheck string) bool {
+	for _, nodeName := range level {
+		if nodeName == nodeToCheck {
+			return true
 		}
 	}
 	return false
