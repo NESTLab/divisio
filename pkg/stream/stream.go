bdeccf99e44ab1a6d1093d62ece03f3b51d0278f
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-03-26 15:22:02 -0400

    Stream updated to work with the new file structure

--- pkg/stream/stream.go
+++ pkg/stream/stream.go
@@ -6,17 +6,21 @@ import (
 	"github.com/NESTLab/divisio.git/pkg/graph"
 	"io/ioutil"
 	"os"
+	"regexp"
 )
 
-func Out(graphs map[string]*graph.Graph, directoryName string) error {
+func GraphOut(graphs map[string]*graph.Graph, directoryName string) error {
 	for name, g := range graphs {
 		//Convert the graph object to json format
 		jsonGraph, err := json.Marshal(g)
 		if err != nil {
 			return err
 		}
-		//Build the graph name. Each graph is named for the group it's in to avoid any duplicates
-		graphName := fmt.Sprintf("%s/graph_%s.json", directoryName, name)
+		//Build the graph name. Each graph is in its own separate folder to group all relevant files together
+		dirName := fmt.Sprintf("%s/graph_%s", directoryName, name)
+		graphName := fmt.Sprintf("%s/graph.json", dirName)
+		os.Mkdir(dirName, os.ModeDir|0777)
+
 		//Create the file
 		fw, err := os.Create(graphName)
 		if err != nil {
@@ -29,24 +33,62 @@ func Out(graphs map[string]*graph.Graph, directoryName string) error {
 	return nil
 }
 
-func In(pathToGraphs string) (map[string]*graph.Graph, error) {
+func ResultOut(results map[string]*graph.Results, directoryName string) error {
+	for name, g := range results {
+		//Convert the graph object to json format
+		jsonGraph, err := json.Marshal(g)
+		if err != nil {
+			return err
+		}
+		//Build the graph name. Each graph is in its own separate folder to group all relevant files together
+		graphName := fmt.Sprintf("%s/graph_%s/result.json", directoryName, name)
+
+		//Create the file
+		fw, err := os.Create(graphName)
+		if err != nil {
+			return err
+		}
+		//Write the json to the file, then close the file
+		fw.Write(jsonGraph)
+		fw.Close()
+	}
+	return nil
+}
+
+func GraphIn(pathToGraphs string) (map[string]*graph.Graph, error) {
+	graphJSONCheck := regexp.MustCompile(`[\w]+.json`)
+	graphNumCheck := regexp.MustCompile(`graph_([\d]+)`)
 	graphs := make(map[string]*graph.Graph)
-	files, err := ioutil.ReadDir(pathToGraphs)
+	runFiles, err := ioutil.ReadDir(pathToGraphs)
 	if err != nil {
 		return graphs, err
 	}
 
-	for _, filename := range files {
-		graphFile, err := ioutil.ReadFile(pathToGraphs + "/" + filename.Name())
-		if err != nil {
-			return graphs, err
+	for _, graphDir := range runFiles {
+		if graphDir.IsDir() {
+			graphNumStr := graphNumCheck.FindStringSubmatch(graphDir.Name())[1]
+			graphFiles, err := ioutil.ReadDir(pathToGraphs + "/" + graphDir.Name())
+			if err != nil {
+				return graphs, err
+			}
+			for _, file := range graphFiles {
+				graphMatch := graphJSONCheck.FindString(file.Name())
+				if graphMatch != "" {
+					fullFileName := pathToGraphs + "/" + graphDir.Name() + "/" + graphMatch
+					var g graph.Graph
+					graphFile, err := ioutil.ReadFile(fullFileName)
+					if err != nil {
+						return graphs, err
+					}
+					err = json.Unmarshal(graphFile, &g)
+					if err != nil {
+						return graphs, err
+					}
+					graphs[graphNumStr] = &g
+				}
+			}
 		}
-		var g graph.Graph
-		err = json.Unmarshal(graphFile, &g)
-		if err != nil {
-			return graphs, err
-		}
-		graphs[filename.Name()] = &g
+
 	}
 	return graphs, nil
 }
