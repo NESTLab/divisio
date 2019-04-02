6afce81c4ff4b25be18636cdc0dd9d05a10d0c49
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-04-02 10:25:51 -0400

    Simpler int -> string conversion

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -17,7 +17,7 @@ func (c Config) GraphBuilderRand(randObj *rand.Rand) *graph.Graph {
 	numNodes := randObj.Intn(c.ExtraNumNodes) + c.BaseNumNodes
 
 	for ii := 0; ii < numNodes; ii++ {
-		name := fmt.Sprintf("%d", ii)
+		name := string(ii)
 
 		if randObj.Float64() < c.ChanceCrossRoads {
 			c.AddNodeRand(&g, randObj, name, true)
